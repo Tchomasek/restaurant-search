ecd6ce8a3ab5282feca5db12c5d744e0386b9f63
Tomas Hruby <tomas.hruby@ixperta.com>
2022-04-22 15:53:29 +0200

    before state

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import CategoryItem from "./src/components/CategoryItem";
+import Header from "./src/components/Header";
+import Search from "./src/components/Search";
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello react native</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
+const commonCategories = [
+  {
+    name: "Burger",
+    imageUrl: require("./src/assets/images/burger.png"),
+  },
+  {
+    name: "Pizza",
+    imageUrl: require("./src/assets/images/pizza.png"),
+  },
+  {
+    name: "Dessert",
+    imageUrl: require("./src/assets/images/cake.png"),
+  },
+  {
+    name: "Pasta",
+    imageUrl: require("./src/assets/images/pasta.png"),
+  },
+  {
+    name: "Drinks",
+    imageUrl: require("./src/assets/images/smoothies.png"),
+  },
+  {
+    name: "Steak",
+    imageUrl: require("./src/assets/images/steak.png"),
+  },
+];
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Search />
+      <View>
+        <FlatList
+          data={commonCategories}
+          renderItem={({ item, index }) => {
+            return (
+              <CategoryItem
+                name={item.name}
+                imageUrl={item.imageUrl}
+                index={index}
+              />
+            );
+          }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(category) => {
+            category.name;
+          }}
+        />
+      </View>
+      <StatusBar />
+    </View>
+  );
+};
 
-export default function App() {}
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.lightHeader}>Grab your</Text>
+      <Text style={styles.boldHeader}>Delicious meal</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 25,
+    marginTop: 60,
+  },
+  lightHeader: {
+    fontSize: 35,
+  },
+  boldHeader: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
+
+export default Header;
